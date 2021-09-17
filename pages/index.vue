278371006bcfa1ec4d9e41bc8d26685a7a1c83d0
chenyitao <chenyitao@zhuanzhuan.com>
2021-09-17 14:21:35 +0800

    update

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="xxx">nuxt-212</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  mounted() {
+    console.log('mounted')
+  },
+})
+</script>
